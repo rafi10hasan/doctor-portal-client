46fa52f174a18499e5b3974e8031d2fb648ea597
Rafi Hasan <rafihasan0075@gmail.com>
2022-10-30 12:00:00 +0600

    code updated

--- src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
+++ src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
@@ -8,7 +8,7 @@ const AppointmentOption = ({ appointmentOption, setTreatment }) => {
                 <h2 className="text-2xl text-secondary font-bold text-center">{name}</h2>
                 <p>{slots.length > 0 ? slots[0] : 'Try Another day'}</p>
                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
-                <p><small>Price: ${price}</small></p>
+                <p><small>price: ${price}</small></p>
                 <div className="card-actions justify-center">
                     <label
                         disabled={slots.length === 0}
